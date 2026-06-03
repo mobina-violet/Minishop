7edea7c9f24410cff693f547628b4530cad9e43e
Mobina Mohagheghi <mobainamohagheghi2002@gmail.com>
2026-06-03 14:52:33 +0330

    add component loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,9 @@
 function Loader() {
   return (
     <div className="flex justify-center items-center py-20">
-      <div className="w-14 h-14 border-4 border-violet-500 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-16 h-16 border-[6px] border-violet-200 border-t-violet-600 rounded-full animate-spin">
+        <p className="text-gray-400 text-sm">Loading...</p>
+      </div>
     </div>
   );
 }
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -2,25 +2,29 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../servises/api";
 import type { Product } from "../types/product";
+import Loader from "../components/Loader";
 //اطلاعات یک محصول خاص رو از API بگیره و نمایش بده.
 function ProductDetails() {
   const { id } = useParams();
 
   const [product, setProduct] = useState<Product | null>(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    api.get(`/products/${id}`).then((res: any) => {
-      setProduct(res.data);
-    });
+    api
+      .get(`/products/${id}`)
+      .then((res: any) => setProduct(res.data))
+      .catch(() => setError(true)); // ← اضافه کن
   }, [id]);
 
+  if (error) return <p>Something went wrong!</p>;
   if (!product) {
-    return <h1>Loading...</h1>;
+    return <Loader />;
   }
   //نمایش کامل یک محصول
   return (
     <div className="max-w-6xl mx-auto p-10 grid md:grid-cols-2 gap-10 ">
-      <div className="bg- rounded-3xl p-10">
+      <div className="bg-white rounded-3xl p-10">
         <img src={product.image} className="w-full  object-contain" />
       </div>
 
